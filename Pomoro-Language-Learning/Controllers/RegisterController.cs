2afced105d9c18b428cd370a2d3f1af755541112
Kevin Tran <kevintran1142@gmail.com>
2022-04-11 11:54:23 -0700

    Setting up registration controller

---
+++ Pomoro-Language-Learning/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pomoro_Language_Learning.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
