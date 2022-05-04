a25c7d3662b10e76d052e8b11942a40507d43335
Kevin Tran <kevintran1142@gmail.com>
2022-05-04 13:23:04 -0700

    Setting up the database to retreive information

---
+++ Pomoro-Language-Learning/Controllers/FlashCardsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pomoro_Language_Learning.Areas.Identity.Data;
+
+namespace Pomoro_Language_Learning.Controllers
+{
+    public class FlashCardsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FlashCardsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.ToList();
+            return View();
+        }
+    }
+}
