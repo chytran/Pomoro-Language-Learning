39bb10cea0d17f3cbf9486b2c7522eab1ee2b0b0
Matt Razo <MatthewRazo16@gmail.com>
2022-05-09 20:29:52 -0700

    Header text added, card WiP on home page

--- Pomoro-Language-Learning/wwwroot/js/site.js
+++ Pomoro-Language-Learning/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// card flip effect
+.flipCard {
+    transform: rotateY(180deg);
+}
+
+const card = document.getElementById("card")
+
+function flipcard() {
+    card.classList.toggle("flipcard");
+}
