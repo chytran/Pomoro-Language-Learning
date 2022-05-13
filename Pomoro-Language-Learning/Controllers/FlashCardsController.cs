4a469a0c9e84efef773b8ef735275208365edbee
Kevin Tran <kevintran1142@gmail.com>
2022-05-12 19:27:41 -0700

    Displays random cards based on how many cards available

--- Pomoro-Language-Learning/Controllers/FlashCardsController.cs
+++ Pomoro-Language-Learning/Controllers/FlashCardsController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pomoro_Language_Learning.Areas.Identity.Data;
-
+using System.Data.SqlClient;
 
 namespace Pomoro_Language_Learning.Controllers
 {
@@ -16,11 +16,22 @@ namespace Pomoro_Language_Learning.Controllers
 
         public IActionResult Index()
         {
+            using (var con = new SqlConnection("Server=tcp:pomoro-server.database.windows.net,1433;Initial Catalog=pomoro-db;Persist Security Info=False;User ID=pomoro-sql;Password=2021graduate!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM FlashCards";
+                using (var cmd = new SqlCommand(query, con))
+                {
+                    int rowsAmount = (int)cmd.ExecuteScalar();
+                    IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw("Select * FROM FlashCards WHERE Id = floor(rand()*" + rowsAmount + ")");
+                    return View(objFlashCardsList);
+                }
+            }
+            // var i = 4;
+            //var count = _db.FlashCards.FromSqlRaw("SELECT COUNT(*) FROM FlashCards");
+            // IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw($"Select * FROM FlashCards WHERE Id = 4{i}")
 
-            var i = 4;
-            IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw($"Select * FROM FlashCards WHERE Id = 4{i}")
-                .ToList();
-            return View(objFlashCardsList);
+            
         }
 
 
