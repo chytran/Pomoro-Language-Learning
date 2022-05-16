a46f570597495a4eb8b5c066c62162104dd75e00
Kevin Tran <kevintran1142@gmail.com>
2022-05-16 13:11:30 -0700

    Setting up get and post request for previous card, next card, and knowing the card.

--- Pomoro-Language-Learning/Controllers/FlashCardsController.cs
+++ Pomoro-Language-Learning/Controllers/FlashCardsController.cs
@@ -81,5 +81,92 @@ namespace Pomoro_Language_Learning.Controllers
         {
             return RedirectToAction("Review");
         }
+
+        // GET
+        public IActionResult Previous()
+        {
+            using (var con = new SqlConnection("Server=tcp:pomoro-server.database.windows.net,1433;Initial Catalog=pomoro-db;Persist Security Info=False;User ID=pomoro-sql;Password=2021graduate!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM FlashCards";
+                using (var cmd = new SqlCommand(query, con))
+                {
+                    int rowsAmount = (int)cmd.ExecuteScalar();
+                    IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw("Select * FROM FlashCards WHERE Id = floor(rand()*" + rowsAmount + ")");
+                    return View(objFlashCardsList);
+                }
+            }
+            // var i = 4;
+            //var count = _db.FlashCards.FromSqlRaw("SELECT COUNT(*) FROM FlashCards");
+            // IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw($"Select * FROM FlashCards WHERE Id = 4{i}")
+
+
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Previous(FlashCards obj)
+        {
+            return RedirectToAction("Review");
+        }
+
+        // GET
+        public IActionResult Check()
+        {
+            using (var con = new SqlConnection("Server=tcp:pomoro-server.database.windows.net,1433;Initial Catalog=pomoro-db;Persist Security Info=False;User ID=pomoro-sql;Password=2021graduate!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM FlashCards";
+                using (var cmd = new SqlCommand(query, con))
+                {
+                    int rowsAmount = (int)cmd.ExecuteScalar();
+                    IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw("Select * FROM FlashCards WHERE Id = floor(rand()*" + rowsAmount + ")");
+                    return View(objFlashCardsList);
+                }
+            }
+            // var i = 4;
+            //var count = _db.FlashCards.FromSqlRaw("SELECT COUNT(*) FROM FlashCards");
+            // IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw($"Select * FROM FlashCards WHERE Id = 4{i}")
+
+
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Check(FlashCards obj)
+        {
+            return RedirectToAction("Review");
+        }
+
+        // GET
+        public IActionResult Next()
+        {
+            using (var con = new SqlConnection("Server=tcp:pomoro-server.database.windows.net,1433;Initial Catalog=pomoro-db;Persist Security Info=False;User ID=pomoro-sql;Password=2021graduate!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM FlashCards";
+                using (var cmd = new SqlCommand(query, con))
+                {
+                    int rowsAmount = (int)cmd.ExecuteScalar();
+                    IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw("Select * FROM FlashCards WHERE Id = floor(rand()*" + rowsAmount + ")");
+                    return View(objFlashCardsList);
+                }
+            }
+            // var i = 4;
+            //var count = _db.FlashCards.FromSqlRaw("SELECT COUNT(*) FROM FlashCards");
+            // IEnumerable<FlashCards> objFlashCardsList = _db.FlashCards.FromSqlRaw($"Select * FROM FlashCards WHERE Id = 4{i}")
+
+
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Next(FlashCards obj)
+        {
+            return RedirectToAction("Review");
+        }
     }
 }
