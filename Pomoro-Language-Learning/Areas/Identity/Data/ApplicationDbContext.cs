5e5ede8d19089310d60bb4c87677eaa020aad53a
Kevin Tran <kevintran1142@gmail.com>
2022-05-18 12:44:53 -0700

    Included another table for database for when the cards are known

--- Pomoro-Language-Learning/Areas/Identity/Data/ApplicationDbContext.cs
+++ Pomoro-Language-Learning/Areas/Identity/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     }
 
     public DbSet<FlashCards> FlashCards { get; set; }
+    public DbSet<ReviewCards> ReviewCards { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
---
+++ Pomoro-Language-Learning/Areas/Identity/Data/ReviewCards.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pomoro_Language_Learning.Areas.Identity.Data
+{
+    public class ReviewCards
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NonTranslated { get; set; }
+        [Required]
+        public string Translated { get; set; }
+    }
+}
